feef4cef86d0f16831a0423c7bb8271d3d449173
Syarif Nurhidayah <syarif.nurhid@gmail.com>
2022-06-05 22:36:23 +0700

    feat : challenge 05 selesai

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = function (data, cbData) {
+  let arrData = cbData(data);
+
+  let result = [];
+  for (let i = 0; i < arrData.length; i++) {
+    result.push(i + 1 + ". " + arrData[i]);
+  }
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = function (param) {
+  return param.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = function (param) {
+  return param.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
